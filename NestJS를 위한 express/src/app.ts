e3fd3126d2e5cb12fe680748099b775028603a97
jerome <jerome@geekstudio.co.kr>
2023-01-28 15:30:30 +0900

    express 미들웨어 이해하기

--- NestJS를 위한 express/src/app.ts
+++ NestJS를 위한 express/src/app.ts
@@ -3,13 +3,37 @@ import { Cat } from './app.model';
 
 const app: express.Express = express();
 
-const port: number = 8000;
+const PORT: number = 8000;
+
+app.use((req, res, next) => {
+    console.log(req.rawHeaders[1]);
+    console.log('this is logging middleware');
+    next();
+});
+
+app.get('/cats/som', (req, res, next) => {
+    console.log('this is som middleware');
+    next();
+});
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    console.log('🚀 ~ file: app.ts:8 ~ app.get ~ req', req);
     res.send({ cats: Cat });
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}/`);
+app.get('/cats/blue', (req, res, next: express.NextFunction) => {
+    res.send({ blue: Cat[0] });
+});
+
+app.get('/cats/som', (req, res) => {
+    res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+    console.log('this is error middleware');
+    next();
+    res.send({ error: '404 not found error' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}/`);
 });
