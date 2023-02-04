33f7533680d90a363fef336e91e14b6b87d84510
jerome <jerome@geekstudio.co.kr>
2023-02-04 16:09:19 +0900

    Providers & 의존성 주입 (DI)

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 소비자(Controller)는 공급자(Provider)에서 제품(Service)을 받아서 사용
+// 아래에서 AppService는 공급자로 취급(Injectable Decorator사용)
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
