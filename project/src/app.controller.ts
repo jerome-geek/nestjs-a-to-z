6ec3d2888dec0e826d46c8e93fa6e4202daa6b96
jerome <jerome@geekstudio.co.kr>
2023-02-04 16:04:36 +0900

    NestJS 구조 & Controller 패턴

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    // localhost:8000/cats/hello
+    @Get('hello/:id/:name')
+    getHello(
+        @Req() req: Request,
+        @Body() body,
+        @Param() param: { ids: string; name: string },
+    ): string {
+        console.log(
+            '🚀 ~ file: app.controller.ts:12 ~ AppController ~ getHello ~ req',
+            req,
+        );
+        return this.appService.getHello();
+    }
 }
